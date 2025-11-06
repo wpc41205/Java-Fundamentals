9256414cc13b7ded41251c260ab296b959b5efcb
FSD9-Wave <loomaflex44@gmail.com>
2025-11-06 09:42:28 +0700

    Add initial Java classes: Main and MyInfo

---
+++ 01-MyInfo/MyInfo.java
@@ -0,0 +1,11 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        String name = "Somchai";
+        int age = 21;
+        String hobby = "playing football";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hobby is " + hobby);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] var0) {
+        System.out.println("Hello, Java from VS Code!");
+    }
+}
